5d608492589e038193834df8624d71b01fb8ea71
RobinVW <robin.vanwelden@gmail.com>
2024-01-10 13:58:11 +0100

    day 2 part 1 solution

---
+++ day2/solution.js
@@ -0,0 +1,48 @@
+const { log } = require('console');
+const fs = require('fs');
+const { it } = require('node:test');
+
+const lines = fs.readFileSync('./input.txt', 'utf8').trim().split('\n');
+
+const content = {
+    'blue': 14,
+    'green': 13,
+    'red': 12
+}
+
+const partOne = () => {
+    const gameMap = new Map();
+    let sum = 0;
+
+    lines.forEach(line => {
+        let splitArray = line.split(':')
+        gameMap.set(Number(splitArray[0].substring(5))
+                   ,splitArray[1].trim());
+    });
+
+    gameMap.forEach((value, key) => {
+        let legitGame = true;
+        value.split(';').forEach(gameSet => {
+            gameSet.split(',').map(grab => {
+                let grabAmount = grab.trim().split(' ');
+                let color = grabAmount[1];
+                let amount = Number(grabAmount[0]);
+                //console.log(`color: ${color} -> ${amount}`);
+                for (const [maxColor, maxAmount] of Object.entries(content)) {
+                    if (color === maxColor && legitGame){
+                        if (amount > maxAmount) {
+                            legitGame = false;
+                        }
+                  }
+                }
+
+            });
+        });
+        if (legitGame) {
+            sum+=key;
+        }
+    });
+    console.log(sum);
+}
+
+partOne();
